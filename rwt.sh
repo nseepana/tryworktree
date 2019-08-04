c5c908043e43661a386ba2f44f2760dff291ca53
nseepana <naresh@seepana.com>
2019-08-05 03:41:31 +0530

    add worktree remove shell and correct branch var

--- rwt.sh
+++ rwt.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+# remove worktree and prune
 END=10;
 #  iterate from 1 to 10<END>
 for i in $(seq 1 $END)
 do 
 	echo $i
 	branch="wtb-$i"
-	git co -b $branch
-	echo -e $branch > notes.txt
-	git acm 'add notes txt'
+	# 'co' is alias for checkout
 	git co master
-	git worktree add "../wt2019/$branch" $branch
+	git worktree remove -f $branch
 done
+git worktree prune
 
