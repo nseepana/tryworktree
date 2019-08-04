e1e179c701d80768f0a48f089d2758a894bd4143
nseepana <naresh@seepana.com>
2019-08-05 03:31:56 +0530

    add notes txt

---
+++ wt.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+END=10;
+
+for i in $(seq 4 $END)
+do 
+	echo $i
+	branch = "wtb-$i";
+	git checkout -b $branch
+	echo -e $branch > notes.txt
+	git acm 'add notes txt'
+	git checkout master
+	git worktree add "../ww2018/$branch" $branch
+done
+
